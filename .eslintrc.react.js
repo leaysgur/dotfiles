7378ba659fa6d217f92a71d7b7d4d7a60f1add23
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-06-20 17:30:02 +0900

    Split eslintrc

--- .eslintrc.react.js
+++ .eslintrc.react.js
@@ -1,13 +1,18 @@
 module.exports = {
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module',
+    ecmaFeatures: { jsx: true },
   },
   plugins: [
+    'react',
+    'import',
   ],
   extends: [
     'eslint:recommended',
     "plugin:prettier/recommended",
+    'plugin:react/recommended',
   ],
   rules: {
     "no-console": "off",
@@ -19,11 +24,16 @@ module.exports = {
     "no-var": "error",
     "no-lonely-if": "error",
     "prefer-const": "error",
+
+    'import/order': 'error',
+
+    'react/jsx-uses-vars': 'warn',
+    'react/prop-types': 'off',
   },
   env: {
     browser: true,
     node: true,
+    jest: true,
     es6: true
   },
 };
-
