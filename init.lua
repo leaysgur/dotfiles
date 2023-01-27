ce6c9845bfdd653ab6a1bbc3c0316db4d8b498f8
Yuji Sugiura <y.sugiura.0316@gmail.com>
2023-01-27 09:38:33 +0900

    Disable default plugins

--- init.lua
+++ init.lua
@@ -23,6 +23,39 @@ vim.opt.updatetime = 500
 -- Disable mouse for term handler
 vim.opt.mouse = ""
 
+-- Disable some builtin vim plugins
+local default_plugins = {
+  "2html_plugin",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "matchit",
+  "tar",
+  "tarPlugin",
+  "rrhelper",
+  "spellfile_plugin",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin",
+  "tutor",
+  "rplugin",
+  "syntax",
+  "synmenu",
+  "optwin",
+  "compiler",
+  "bugreport",
+  "ftplugin",
+}
+for _, plugin in pairs(default_plugins) do
+  vim.g["loaded_" .. plugin] = 1
+end
+
 
 local map_args = { silent = true, noremap = true };
 -- Keep visual mode after indentation
