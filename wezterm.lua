797effe1c4c0579cd5c288e16ead6a9e2cc30248
Yuji Sugiura <y.sugiura.0316@gmail.com>
2023-12-27 10:48:43 +0900

    Fix wezterm settings

--- wezterm.lua
+++ wezterm.lua
@@ -1,26 +1,30 @@
-local wezterm = require("wezterm")
-local act = wezterm.action
+local w = require("wezterm")
 
 return {
-	-- https://github.com/be5invis/Sarasa-Gothic
-	font = wezterm.font("Sarasa Fixed J"),
-	font_size = 18,
-	adjust_window_size_when_changing_font_size = false,
-	initial_cols = 200,
-	initial_rows = 50,
 	color_scheme = "Oceanic-Next",
-	window_background_opacity = 0.8,
-	text_background_opacity = 0.6,
-	inactive_pane_hsb = { brightness = 0.2 },
+	window_background_opacity = 0.9,
+	text_background_opacity = 0.7,
+
+	-- https://github.com/be5invis/Sarasa-Gothic
+	font = w.font("Sarasa Fixed J"),
+	font_size = 20,
+	adjust_window_size_when_changing_font_size = false,
+
+	initial_cols = 180,
+	initial_rows = 50,
+
+	inactive_pane_hsb = { brightness = 0.3 },
 	hide_tab_bar_if_only_one_tab = true,
 	tab_bar_at_bottom = true,
+
 	keys = {
 		-- This makes (Neo)Vim key map `<C-_>`(slash) works
-		{ key = "/", mods = "CTRL", action = act({ SendString = "\x1f" }) },
-		{ key = "D", mods = "SUPER", action = act.SplitVertical },
-		{ key = "d", mods = "SUPER", action = act.SplitHorizontal },
-		{ key = "[", mods = "SUPER", action = act({ ActivatePaneDirection = "Prev" }) },
-		{ key = "]", mods = "SUPER", action = act({ ActivatePaneDirection = "Next" }) },
+		{ key = "/", mods = "CTRL", action = w.action({ SendString = "\x1f" }) },
+		{ key = "D", mods = "SUPER", action = w.action.SplitVertical },
+		{ key = "d", mods = "SUPER", action = w.action.SplitHorizontal },
+		{ key = "[", mods = "SUPER", action = w.action({ ActivatePaneDirection = "Prev" }) },
+		{ key = "]", mods = "SUPER", action = w.action({ ActivatePaneDirection = "Next" }) },
 	},
+
 	-- debug_key_events = true,
 }
