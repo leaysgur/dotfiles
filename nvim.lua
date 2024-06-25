ad1c022d19c4146d466262956c3bba74015db47c
Yuji Sugiura <y.sugiura.0316@gmail.com>
2024-06-25 09:07:32 +0900

    Follow lazy.nvim updates

--- nvim.lua
+++ nvim.lua
@@ -1,3 +1,14 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 -- Ui
 vim.opt.number = true
 vim.opt.list = true
@@ -31,27 +42,16 @@ vim.keymap.set("n", "<Esc>", ":nohlsearch<CR><Esc>", keymap_opts)
 -- See https://github.com/LazyVim/LazyVim/discussions/1583
 local LazyFile = { "BufReadPost", "BufNewFile", "BufWritePre" }
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
+-- Setup lazy.nvim
 require("lazy").setup({
 	-- Theme
 	{
 		"fynnfluegge/monet.nvim",
 		name = "monet",
+		lazy = false,
 		priority = 1000,
 		-- stylua: ignore
-		config = function() vim.cmd("colorscheme monet") end,
+		config = function() vim.cmd([[colorscheme monet]]) end,
 	},
 
 	-- Uis
