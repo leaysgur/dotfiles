81aad8902b684005ba1a99af6538a70b23e448bd
Yuji Sugiura <y.sugiura.0316@gmail.com>
2024-10-01 11:00:24 +0900

    Update wezterm styles

--- wezterm.lua
+++ wezterm.lua
@@ -2,8 +2,10 @@ local w = require("wezterm")
 
 return {
 	color_scheme = "Ayu Mirage",
-	window_background_opacity = 0.8,
-	text_background_opacity = 0.8,
+	window_background_opacity = 0.7,
+	text_background_opacity = 0.5,
+	macos_window_background_blur = 20,
+	inactive_pane_hsb = { brightness = 0.3 },
 
 	-- https://github.com/be5invis/Sarasa-Gothic
 	font = w.font("Sarasa Fixed J Light"),
@@ -13,9 +15,10 @@ return {
 	initial_cols = 240,
 	initial_rows = 56,
 
-	inactive_pane_hsb = { brightness = 0.3 },
+	window_decorations = "RESIZE",
 	hide_tab_bar_if_only_one_tab = true,
-	tab_bar_at_bottom = true,
+	show_new_tab_button_in_tab_bar = false,
+	show_close_tab_button_in_tabs = false,
 
 	keys = {
 		-- This makes (Neo)Vim key map `<C-_>`(slash) works
