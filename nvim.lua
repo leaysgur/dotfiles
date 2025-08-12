5b916f2d1d994fb701c60032d5958cc2635e5bc0
Yuji Sugiura <y.sugiura.0316@gmail.com>
2025-08-12 16:00:28 +0900

    Wip

--- nvim.lua
+++ nvim.lua
@@ -48,7 +48,7 @@ now(function()
 	vim.keymap.set("n", "<Esc>", ":nohlsearch<CR><Esc>", { silent = true })
 end)
 
--- # Theme
+-- # Theme, UIs
 now(function()
 	add({ source = "everviolet/nvim", name = "evergarden" })
 	require("evergarden").setup({
@@ -56,14 +56,175 @@ now(function()
 		editor = { transparent_background = true },
 	})
 	vim.cmd([[colorscheme evergarden]])
+end)
 
+now(function()
 	add({ source = "echasnovski/mini.icons" })
 	require("mini.icons").setup()
 	require("mini.icons").tweak_lsp_kind()
 	require("mini.icons").mock_nvim_web_devicons()
 end)
 
--- # LSP, Completion
+now(function()
+	add({ source = "echasnovski/mini.notify" })
+	require("mini.notify").setup()
+	vim.notify = require("mini.notify").make_notify()
+end)
+
+now(function()
+	add({ source = "bluz71/nvim-linefly" })
+	vim.g.linefly_options = { with_search_count = true }
+end)
+
+now(function()
+	add({ source = "b0o/incline.nvim" })
+	require("incline").setup()
+end)
+
+now(function()
+	add({
+		source = "nvim-neo-tree/neo-tree.nvim",
+		checkout = "v3.x",
+		depends = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+	})
+	require("neo-tree").setup({
+		window = {
+			mappings = {
+				["<Space>"] = { "toggle_node", nowait = true },
+				["<C-s>"] = "open_split",
+				["<C-v>"] = "open_vsplit",
+			},
+		},
+		filesystem = {
+			filtered_items = { visible = true },
+			hijack_netrw_behavior = "open_current",
+		},
+	})
+end)
+
+now(function()
+	add({ source = "folke/snacks.nvim" })
+	require("snacks").setup({
+		bigfile = {},
+		indent = {},
+		picker = {}, -- Used by `codecompanion.nvim`
+		words = { debounce = 50 },
+	})
+	vim.keymap.set("n", "[w", ":lua Snacks.words.jump(-vim.v.count1, true)<CR>", { silent = true })
+	vim.keymap.set("n", "]w", ":lua Snacks.words.jump(vim.v.count1, true)<CR>", { silent = true })
+end)
+
+-- # UX
+later(function()
+	add({ source = "ibhagwan/smartyank.nvim" })
+	require("smartyank").setup({ highlight = { timeout = 80 } })
+end)
+
+later(function()
+	add({ source = "sphamba/smear-cursor.nvim" })
+	require("smear_cursor").setup({ color_levels = 8, gamma = 4 })
+end)
+
+later(function()
+	add({ source = "rainbowhxch/accelerated-jk.nvim" })
+	require("accelerated-jk").setup()
+	vim.keymap.set("n", "<Up>", "<Plug>(accelerated_jk_gk)", { silent = true })
+	vim.keymap.set("n", "<Down>", "<Plug>(accelerated_jk_gj)", { silent = true })
+end)
+
+later(function()
+	add({ source = "nvim-focus/focus.nvim" })
+	require("focus").setup({ commands = false })
+end)
+
+later(function()
+	add({ source = "catgoose/nvim-colorizer.lua" })
+	require("colorizer").setup({
+		user_commands = false,
+		user_default_options = { css = true, tailwind = true },
+	})
+end)
+
+later(function()
+	add({ source = "echasnovski/mini.diff" })
+	require("mini.diff").setup()
+end)
+
+-- # Treesitter
+-- now(function()
+-- 	add({
+-- 		source = "nvim-treesitter/nvim-treesitter",
+-- 		checkout = "master",
+-- 		depends = {
+-- 			"JoosepAlviste/nvim-ts-context-commentstring",
+-- 			"nvim-treesitter/nvim-treesitter-context",
+-- 			"windwp/nvim-ts-autotag",
+-- 			"theHamsta/nvim-treesitter-pairs",
+-- 		},
+-- 	})
+-- 	require("nvim-treesitter.configs").setup({
+-- 		ensure_installed = "all",
+-- 		ignore_install = { "ipkg" },
+-- 		highlight = {
+-- 			enable = true,
+-- 			additional_vim_regex_highlighting = false,
+-- 		},
+-- 		indent = { enable = true },
+-- 		pairs = {
+-- 			enable = true,
+-- 			fallback_cmd_normal = "normal! %",
+-- 			keymaps = { goto_partner = "%" },
+-- 		},
+-- 	})
+-- 	vim.cmd("TSUpdate")
+
+-- 	require("ts_context_commentstring").setup({ enable_autocmd = false })
+-- 	require("treesitter-context").setup({ max_lines = 1 })
+-- 	require("nvim-ts-autotag").setup()
+-- end)
+
+-- now(function()
+-- 	add({ source = "terrortylor/nvim-comment" })
+-- 	require("nvim_comment").setup({
+-- 		create_mappings = false,
+-- 		comment_empty = false,
+-- 		hook = function() require("ts_context_commentstring").update_commentstring() end,
+-- 	})
+-- 	vim.keymap.set("n", "<C-_>", ":CommentToggle<CR>", { silent = true })
+-- 	vim.keymap.set("v", "<C-_>", ":'<,'>CommentToggle<CR>", { silent = true })
+-- end)
+
+-- now(function()
+-- 	add({ source = "Wansmer/treesj" })
+-- 	require("treesj").setup({ use_default_keymaps = false })
+-- 	vim.keymap.set("n", "sj", ":TSJToggle<CR>", { silent = true })
+-- end)
+
+-- # Editing
+later(function()
+	add({ source = "echasnovski/mini.surround" })
+	require("mini.surround").setup()
+end)
+
+later(function()
+	add({ source = "monkoose/matchparen.nvim" })
+	require("matchparen").setup()
+end)
+
+later(function()
+	add({ source = "windwp/nvim-autopairs" })
+	require("nvim-autopairs").setup()
+end)
+
+later(function()
+	add({ source = "Darazaki/indent-o-matic" })
+	require("indent-o-matic").setup({})
+end)
+
+-- # Language server, linter, formatter
 now(function()
 	add({
 		source = "neovim/nvim-lspconfig",
@@ -109,24 +270,54 @@ now(function()
 end)
 
 later(function()
-	add({
-		source = "echasnovski/mini.completion",
-	})
-	require("mini.completion").setup({
-		lsp_completion = { source_func = "omnifunc" },
-	})
-
-	vim.keymap.set("i", "<Tab>", [[pumvisible() ? "\<Down>" : "\<Tab>"]], { expr = true, replace_keycodes = false })
-	vim.keymap.set("i", "<S-Tab>", [[pumvisible() ? "\<Up>" : "\<S-Tab>"]], { expr = true, replace_keycodes = false })
-end)
-
-later(function()
 	add({ source = "rachartier/tiny-inline-diagnostic.nvim" })
 	require("tiny-inline-diagnostic").setup({
 		preset = "nonerdfont",
 		signs = { arrow = "", up_arrow = "" }, -- Arrows are useless because column pos is not moved
 		options = { show_source = true },
 	})
+end)
+
+later(function()
+	add({ source = "dnlhc/glance.nvim" })
+	local glance = require("glance")
+	glance.setup({
+		height = 30,
+		list = { position = "left" },
+		folds = { folded = false },
+		border = { enable = true },
+		mappings = {
+			list = {
+				["<C-s>"] = glance.actions.jump_split,
+				["<C-v>"] = glance.actions.jump_vsplit,
+				["<C-w><Right>"] = glance.actions.enter_win("preview"),
+			},
+			preview = {
+				["<C-w><Left>"] = glance.actions.enter_win("list"),
+			},
+		},
+	})
+	vim.keymap.set("n", "gd", ":Glance definitions<CR>", { silent = true })
+	vim.keymap.set("n", "gr", ":Glance references<CR>", { silent = true })
+end)
+
+later(function()
+	add({
+		source = "hedyhli/outline.nvim",
+		depends = { "echasnovski/mini.icons" },
+	})
+	require("outline").setup({
+		outline_window = { width = 50 },
+		outline_items = { show_symbol_lineno = true },
+		keymaps = { fold_toggle = "<Space>" },
+		symbol_folding = { autofold_depth = 2 },
+		symbols = {
+			icon_fetcher = function(kind)
+				return require("mini.icons").get("lsp", kind) .. " " .. kind
+			end,
+		},
+	})
+	vim.api.nvim_create_user_command("OO", "Outline", {})
 end)
 
 later(function()
@@ -142,245 +333,75 @@ later(function()
 	vim.keymap.set("n", "<Space>f", ":lua require('conform').format()<CR>", { silent = true })
 end)
 
--- 	-- ## UI/UX
--- 	{
--- 		"bluz71/nvim-linefly",
--- 		--stylua: ignore
--- 		init = function() vim.g.linefly_options = {
--- 				with_search_count = true,
--- 		} end,
--- 		-- Do not lazy load, just leave it to plugin
--- 	},
--- 	{ "b0o/incline.nvim", config = true, event = LazyFile },
--- 	{
--- 		"folke/snacks.nvim",
--- 		priority = 1000,
--- 		opts = {
--- 			bigfile = {},
--- 			indent = {},
--- 			picker = {}, -- For `CodeCompanion.nvim` to change adapters
--- 			words = { debounce = 50 },
--- 		},
--- 		init = function()
--- 			vim.keymap.set("n", "[w", ":lua Snacks.words.jump(-vim.v.count1, true)<CR>", { silent = true })
--- 			vim.keymap.set("n", "]w", ":lua Snacks.words.jump(vim.v.count1, true)<CR>", { silent = true })
--- 		end,
--- 		lazy = false,
--- 	},
--- 	{
--- 		"echasnovski/mini.notify",
--- 		config = true,
--- 		-- stylua: ignore
--- 		init = function() vim.notify = require("mini.notify").make_notify() end,
--- 		event = LazyFile,
--- 	},
--- 	{
--- 		"ibhagwan/smartyank.nvim",
--- 		opts = { highlight = { timeout = 80 } },
--- 		event = LazyFile,
--- 	},
--- 	{
--- 		"sphamba/smear-cursor.nvim",
--- 		opts = { color_levels = 8, gamma = 4 },
--- 		event = LazyFile,
--- 	},
--- 	{
--- 		"rainbowhxch/accelerated-jk.nvim",
--- 		config = true,
--- 		keys = {
--- 			{ "<Up>", "<Plug>(accelerated_jk_gk)", silent = true },
--- 			{ "<Down>", "<Plug>(accelerated_jk_gj)", silent = true },
--- 		},
--- 	},
--- 	{
--- 		"nvim-focus/focus.nvim",
--- 		opts = { commands = false },
--- 		event = LazyFile,
--- 	},
--- 	{ "echasnovski/mini.diff", config = true, event = LazyFile },
--- 	{
--- 		"nvim-neo-tree/neo-tree.nvim",
--- 		branch = "v3.x",
--- 		dependencies = {
--- 			"nvim-lua/plenary.nvim",
--- 			"MunifTanjim/nui.nvim",
--- 		},
--- 		opts = {
--- 			window = {
--- 				mappings = {
--- 					["<Space>"] = { "toggle_node", nowait = true },
--- 					["<C-s>"] = "open_split",
--- 					["<C-v>"] = "open_vsplit",
--- 				},
--- 			},
--- 			filesystem = {
--- 				filtered_items = { visible = true },
--- 				hijack_netrw_behavior = "open_current",
--- 			},
--- 		},
--- 		-- To open directory like `netrw`, this plugin cannot be lazy loaded
--- 	},
--- 	{
--- 		"catgoose/nvim-colorizer.lua",
--- 		opts = {
--- 			user_commands = false,
--- 			user_default_options = { css = true, tailwind = true },
--- 		},
--- 		ft = { "css", "html", "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte", "astro" },
--- 	},
+-- # Completion
+later(function()
+	add({
+		source = "echasnovski/mini.completion",
+	})
+	require("mini.completion").setup({
+		lsp_completion = { source_func = "omnifunc" },
+	})
 
--- 	-- ## Treesitter
--- 	{
--- 		"nvim-treesitter/nvim-treesitter",
--- 		branch = "master",
--- 		main = "nvim-treesitter.configs",
--- 		opts = {
--- 			ensure_installed = "all",
--- 			ignore_install = { "ipkg" },
--- 			highlight = {
--- 				enable = true,
--- 				additional_vim_regex_highlighting = false,
--- 			},
--- 			indent = { enable = true },
--- 			-- Enable `nvim-treesitter-pairs`
--- 			pairs = {
--- 				enable = true,
--- 				fallback_cmd_normal = "normal! %",
--- 				keymaps = { goto_partner = "%" },
--- 			},
--- 		},
--- 		build = ":TSUpdate",
--- 		-- Does not support lazy loading
--- 	},
--- 	{ "JoosepAlviste/nvim-ts-context-commentstring", opts = { enable_autocmd = false }, event = LazyFile },
--- 	{ "nvim-treesitter/nvim-treesitter-context", opts = { max_lines = 1 }, event = LazyFile },
--- 	{ "windwp/nvim-ts-autotag", config = true, event = LazyFile },
--- 	{ "theHamsta/nvim-treesitter-pairs", event = LazyFile },
--- 	{
--- 		"terrortylor/nvim-comment",
--- 		main = "nvim_comment",
--- 		opts = {
--- 			create_mappings = false,
--- 			comment_empty = false,
--- 			--stylua: ignore
--- 			hook = function() require("ts_context_commentstring").update_commentstring() end,
--- 		},
--- 		keys = {
--- 			{ "<C-_>", ":CommentToggle<CR>", silent = true },
--- 			{ "<C-_>", ":'<,'>CommentToggle<CR>", mode = "v", silent = true },
--- 		},
--- 	},
--- 	{
--- 		"Wansmer/treesj",
--- 		opts = { use_default_keymaps = false },
--- 		keys = { { "sj", ":TSJToggle<CR>", silent = true } },
--- 	},
+	vim.keymap.set("i", "<Tab>", [[pumvisible() ? "\<Down>" : "\<Tab>"]], { expr = true, replace_keycodes = false })
+	vim.keymap.set("i", "<S-Tab>", [[pumvisible() ? "\<Up>" : "\<S-Tab>"]], { expr = true, replace_keycodes = false })
+end)
 
--- 	-- ## Editing
--- 	{ "Darazaki/indent-o-matic", config = true, event = LazyFile },
--- 	{ "echasnovski/mini.surround", config = true, event = LazyFile },
--- 	{ "monkoose/matchparen.nvim", config = true, event = LazyFile },
--- 	{ "windwp/nvim-autopairs", config = true, event = "InsertEnter" },
--- 	{
--- 		"zbirenbaum/copilot.lua",
--- 		opts = {
--- 			suggestion = {
--- 				auto_trigger = true,
--- 				keymap = {
--- 					accept = "<c-CR>",
--- 					next = "<c-]>",
--- 					prev = "<c-[>",
--- 					dismiss = "<ESC>",
--- 				},
--- 			},
--- 			panel = { enabled = false },
--- 		},
--- 		event = "InsertEnter",
--- 	},
--- 	{
--- 		"olimorris/codecompanion.nvim",
--- 		dependencies = {
--- 			"nvim-lua/plenary.nvim",
--- 			"nvim-treesitter/nvim-treesitter",
--- 		},
--- 		opts = {
--- 			strategies = { chat = { adapter = "anthropic" } },
--- 			adapters = {
--- 				copilot = function()
--- 					return require("codecompanion.adapters").extend(
--- 						"copilot",
--- 						{ schema = { model = { default = "claude-3.7-sonnet-thought" } } }
--- 					)
--- 				end,
--- 				anthropic = function()
--- 					return require("codecompanion.adapters").extend(
--- 						"anthropic",
--- 						{ schema = { model = { default = "claude-opus-4-20250514" } } }
--- 					)
--- 				end,
--- 			},
--- 			display = { chat = { show_header_separator = true } },
--- 			opts = { language = "same" },
--- 		},
--- 		init = function()
--- 			vim.api.nvim_create_autocmd({ "User" }, {
--- 				pattern = "CodeCompanionRequest{Started,Streaming,Finished}",
--- 				group = vim.api.nvim_create_augroup("CodeCompanionHooks", { clear = true }),
--- 				callback = function(request)
--- 					vim.notify("CodeCompanion: " .. request.match:gsub("CodeCompanion", ""), vim.log.levels.INFO, {
--- 						id = "code_companion_status",
--- 						title = "CodeCompanion.nvim",
--- 						timeout = 1600,
--- 						-- stylua: ignore
--- 						keep = function() return not vim.endswith(request.match, "Finished") end,
--- 					})
--- 				end,
--- 			})
--- 		end,
--- 		cmd = { "CodeCompanion", "CodeCompanionChat" },
--- 	},
+later(function()
+	add({ source = "zbirenbaum/copilot.lua" })
+	require("copilot").setup({
+		suggestion = {
+			auto_trigger = true,
+			keymap = {
+				accept = "<c-CR>",
+				next = "<c-]>",
+				prev = "<c-[>",
+				dismiss = "<ESC>",
+			},
+		},
+		panel = { enabled = false },
+	})
+end)
 
--- 	-- ## LSP
--- 	{
--- 		"dnlhc/glance.nvim",
--- 		config = function()
--- 			local glance = require("glance")
--- 			glance.setup({
--- 				height = 30,
--- 				list = { position = "left" },
--- 				folds = { folded = false },
--- 				border = { enable = true },
--- 				mappings = {
--- 					list = {
--- 						["<C-s>"] = glance.actions.jump_split,
--- 						["<C-v>"] = glance.actions.jump_vsplit,
--- 						["<C-w><Right>"] = glance.actions.enter_win("preview"),
--- 					},
--- 					preview = {
--- 						["<C-w><Left>"] = glance.actions.enter_win("list"),
--- 					},
--- 				},
--- 			})
--- 		end,
--- 		keys = {
--- 			{ "gd", ":Glance definitions<CR>", silent = true },
--- 			{ "gr", ":Glance references<CR>", silent = true },
--- 		},
--- 	},
--- 	{
--- 		"hedyhli/outline.nvim",
--- 		opts = {
--- 			outline_window = { width = 50 },
--- 			outline_items = { show_symbol_lineno = true },
--- 			keymaps = { fold_toggle = "<Space>" },
--- 			symbol_folding = { autofold_depth = 2 },
--- 			symbols = {
--- 				icon_fetcher = function(kind)
--- 					return require("mini.icons").get("lsp", kind) .. " " .. kind
--- 				end,
--- 			},
--- 		},
--- 		--stylua: ignore
---     init = function() vim.api.nvim_create_user_command("OO", "Outline", {}) end,
--- 		cmd = "Outline",
--- 	},
+later(function()
+	add({
+		source = "olimorris/codecompanion.nvim",
+		depends = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+	})
+	require("codecompanion").setup({
+		strategies = { chat = { adapter = "anthropic" } },
+		adapters = {
+			copilot = function()
+				return require("codecompanion.adapters").extend(
+					"copilot",
+					{ schema = { model = { default = "claude-3.7-sonnet-thought" } } }
+				)
+			end,
+			anthropic = function()
+				return require("codecompanion.adapters").extend(
+					"anthropic",
+					{ schema = { model = { default = "claude-opus-4-20250514" } } }
+				)
+			end,
+		},
+		display = { chat = { show_header_separator = true } },
+		opts = { language = "same" },
+	})
+
+	vim.api.nvim_create_autocmd({ "User" }, {
+		pattern = "CodeCompanionRequest{Started,Streaming,Finished}",
+		group = vim.api.nvim_create_augroup("CodeCompanionHooks", { clear = true }),
+		callback = function(request)
+			vim.notify("CodeCompanion: " .. request.match:gsub("CodeCompanion", ""), vim.log.levels.INFO, {
+				id = "code_companion_status",
+				title = "CodeCompanion.nvim",
+				timeout = 1600,
+				keep = function()
+					return not vim.endswith(request.match, "Finished")
+				end,
+			})
+		end,
+	})
+end)
